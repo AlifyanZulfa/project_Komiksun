9e2c10f8caeecc37b8cc601b71cf7f8b7da893ab
Alifyan Zulfa <alifyan.zulfa6661@gmail.com>
2022-06-05 22:14:37 +0700

    commit Alifyan

---
+++ src/scripts/index.js
@@ -0,0 +1,24 @@
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/main.css';
+import './component/app-bar';
+import './component/app-footer';
+import './component/app-search';
+import './component/app-category-bar';
+import './component/app-category';
+import App from './views/app';
+
+const app = new App({
+  jumbotron: document.querySelector('#jumbotron'),
+  content: document.querySelector('#mainContent'),
+  searchElement: document.querySelector('app-search'),
+  searchListElement: document.querySelector('search-list'),
+});
+
+window.addEventListener('hashchange', () => {
+  app.renderPage();
+});
+
+window.addEventListener('load', () => {
+  app.renderPage();
+});
